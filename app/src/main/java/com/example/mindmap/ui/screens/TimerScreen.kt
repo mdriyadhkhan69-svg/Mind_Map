349aea7550de381cedcd69908b7fb366f96a432e
MD Riyadh Khan <mdriyadhkhan69@gmail.com>
2026-08-23 13:47:10 +0600

    Update TimerScreen.kt

--- app/src/main/java/com/example/mindmap/ui/screens/TimerScreen.kt
+++ app/src/main/java/com/example/mindmap/ui/screens/TimerScreen.kt
@@ -42,7 +42,7 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.GenericShape
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
